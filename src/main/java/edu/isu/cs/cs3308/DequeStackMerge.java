b43974a64ae1ac7751c37ec708da3645cee2b185
baumetha <46669882+baumetha@users.noreply.github.com>
2019-02-10 01:27:06 -0700

    Add files via upload

--- src/main/java/edu/isu/cs/cs3308/DequeStackMerge.java
+++ src/main/java/edu/isu/cs/cs3308/DequeStackMerge.java
@@ -5,7 +5,8 @@ import edu.isu.cs.cs3308.structures.Stack;
 import edu.isu.cs.cs3308.structures.impl.LinkedDeque;
 
 /**
- *
+ * @author Ethan Baumgartner
+ * Merges to stacks using a deque
  * @author Isaac Griffith
  */
 public class DequeStackMerge {
@@ -20,6 +21,23 @@ public class DequeStackMerge {
      * @param from Stack which is merged into the bottom of the other stack.
      */
     public static <E> void dequeStackMerge(final Stack<E> into, Stack<E> from) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        /**
+         * Pops off the elements from both stacks and stores them in a temporary Deque with
+         * the elements from the from stack under the elements in into, then pushes the last element to
+         * the into stack until temp is empty
+         */
+        if (into == null || from == null){
+            return;
+        }
+        LinkedDeque<E> temp = new LinkedDeque<>();
+        while (into.size > 0()){
+            temp.offer(into.pop());
+        }
+        while(from.size > 0()){
+            temp.offer(from.pop());
+        }
+        while (tempDeque.size > 0()){
+            into.push(temp.pollLast());
+        }
     }
 }
